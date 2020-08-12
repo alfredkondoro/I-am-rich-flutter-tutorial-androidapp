268238914c14e8bbe429ecb021b70deb152437fa
Alfred Kondoro <alfredkondoro97@gmail.com>
2020-08-12 21:53:23 +0300

    version 0.1

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// Starting point for all flutter apps
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
